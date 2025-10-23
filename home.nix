30eceeabd51e4e7bba1e843445ed8702174743a6
Asteria Louven <asteria@louven.me>
2025-10-23 17:10:36 +0200

    initial commit

---
+++ home.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+home.username = "asteria";
+home.homeDirectory = "/home/asteria";
+
+home.packages = with pkgs; [
+neofetch
+nnn
+zip
+unzip
+file
+tree
+which
+nix-output-monitor
+glow
+btop
+lsof
+ltrace
+lm_sensors
+pciutils
+usbutils
+];
+
+home.stateVersion = "25.05";
+
+programs.git = {
+enable = true;
+userName = "Asteria Louven";
+userEmail = "asteria@louven.me";
+};
+
+programs.docker-cli = {
+enable = true;
+};
+}
