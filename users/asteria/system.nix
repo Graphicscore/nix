4634ff88d7f72b54144b497730651e10aadd1cb6
Asteria Louven <asteria@louven.me>
2025-10-24 19:21:30 +0200

    update nix config

--- users/asteria/system.nix
+++ users/asteria/system.nix
@@ -7,7 +7,7 @@
       isNormalUser = true;
       home = "/home/asteria";
       createHome = true;
-      passwordFile = "/persist/encrypted-passwords/asteria";
+      #passwordFile = "/persist/encrypted-passwords/asteria";
       extraGroups = [ "wheel" "disk" "networkmanager" "libvirtd" "qemu-libvirtd" "kvm" ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICApdJZ55ToUq4FgBBL0yq0NAwW+PLBBLjH+SN29x21w asteria@asteria-MS-7D75"
