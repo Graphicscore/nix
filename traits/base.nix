86f18fb50f684b56247c164af4e4bf57bc9c288d
Asteria Louven <asteria@louven.me>
2025-11-04 18:11:14 +0100

    update nix config

--- traits/base.nix
+++ traits/base.nix
@@ -2,8 +2,80 @@
 
 {
   config = {
+
+
+    services = {
+      desktopManager.plasma6.enable = true;
+
+      displayManager.sddm.enable = true;
+
+      displayManager.sddm.wayland.enable = true;
+    };
+
+
+    # rtkit is optional but recommended
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+    };
+
+    services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
+      "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      };
+    };
+
+     services.pipewire.extraConfig.pipewire."92-low-latency" = {
+    "context.properties" = {
+      "default.clock.rate" = 48000;
+      "default.clock.quantum" = 32;
+      "default.clock.min-quantum" = 32;
+      "default.clock.max-quantum" = 32;
+    };
+  };
+
+  services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
+  context.modules = [
+    {
+      name = "libpipewire-module-protocol-pulse";
+      args = {
+        pulse.min.req = "32/48000";
+        pulse.default.req = "32/48000";
+        pulse.max.req = "32/48000";
+        pulse.min.quantum = "32/48000";
+        pulse.max.quantum = "32/48000";
+      };
+    }
+  ];
+  stream.properties = {
+    node.latency = "32/48000";
+    resample.quality = 1;
+  };
+};
+
+
     time.timeZone = "Europe/Berlin";
     i18n.defaultLocale = "de_DE.UTF-8";
+
+    environment.plasma6.excludePackages = with pkgs; [
+      kdePackages.elisa # Simple music player aiming to provide a nice experience for its users
+      kdePackages.kdepim-runtime # Akonadi agents and resources
+      kdePackages.kmahjongg # KMahjongg is a tile matching game for one or two players
+      kdePackages.kmines # KMines is the classic Minesweeper game
+      kdePackages.konversation # User-friendly and fully-featured IRC client
+      kdePackages.kpat # KPatience offers a selection of solitaire card games
+      kdePackages.ksudoku # KSudoku is a logic-based symbol placement puzzle
+      kdePackages.ktorrent # Powerful BitTorrent client
+      mpv
+    ];
 
     environment.systemPackages = with pkgs; [
       # Shell utilities
@@ -21,7 +93,9 @@
 #      simple-http-server
       curl
       wget
-#      grep
+      wl-clipboard
+      kdePackages.sddm-kcm # Configuration module for SDDM
+      
     ];
     #environment.shellAliases = { };
     #environment.variables = {
