ed47e8f3712294f9729741654d9763d7df34da52
Asteria Louven <asteria@louven.me>
2025-11-04 21:39:02 +0100

    update nix config

--- modules/plasma/itchy-theme.nix
+++ modules/plasma/itchy-theme.nix
@@ -1,15 +1,17 @@
-{lib, stdenv, fetchgit}:
+{config, lib, pkgs, ...}:
 
-
-stdenv.mkDerivation {
-  name = "itchy-theme";
-  version = "latest";
-  src = fetchgit {
-    url = "https://gitlab.com/jomada/Itchy";
-    rev = "4e2f1f224dceae4f4625e66732919fb93ce6145c";
+{
+  itchy-theme = pkgs.mkDerivation {
+    pname = "itchy-theme";
+    version = "latest";
+    src = pkgs.fetchgit {
+      url = "https://gitlab.com/jomada/Itchy";
+      rev = "4e2f1f224dceae4f4625e66732919fb93ce6145c";
+      hash = "sha-256-w3D5dZPBhuNiqQXY4g8zngk3hGbGaPSVYSbSSX08RLw="
+    };
+    installPhase = ''
+      mkdir -p $out/share/plasma
+      cp -aR * $out/share/plasma
+    '';
   };
-  installPhase = ''
-    mkdir -p $out/share/plasma
-    cp -aR * $out/share/plasma
-  '';
 }
