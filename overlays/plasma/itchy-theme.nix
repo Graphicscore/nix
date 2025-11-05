4bab57a7f61aef2aa556ba3026df744f21e3034d
Asteria Louven <asteria@louven.me>
2025-11-05 19:41:04 +0100

    update nix config

--- overlays/plasma/itchy-theme.nix
+++ overlays/plasma/itchy-theme.nix
@@ -7,7 +7,7 @@
     src = pkgs.fetchgit {
       url = "https://gitlab.com/jomada/Itchy";
       rev = "4e2f1f224dceae4f4625e66732919fb93ce6145c";
-      hash = "sha-256-w3D5dZPBhuNiqQXY4g8zngk3hGbGaPSVYSbSSX08RLw="
+      hash = "sha-256-w3D5dZPBhuNiqQXY4g8zngk3hGbGaPSVYSbSSX08RLw=";
     };
     installPhase = ''
       mkdir -p $out/share/plasma
