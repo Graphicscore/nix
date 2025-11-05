753579c6c2bef3b3aac9b6ca9a2fb0355c0dbafc
Asteria Louven <asteria@louven.me>
2025-11-05 20:02:02 +0100

    update nix config

--- overlays/plasma/itchy-theme.nix
+++ overlays/plasma/itchy-theme.nix
@@ -3,7 +3,7 @@ final: prev: {
     pname = "itchy-theme";
     version = "latest";
     src = prev.fetchgit {
-      url = "https://gitlab.com/jomada/Itchy";
+      url = "https://gitlab.com/jomada/Itchy.git";
       rev = "4e2f1f224dceae4f4625e66732919fb93ce6145c";
       hash = "sha256-w3D5dZPBhuNiqQXY4g8zngk3hGbGaPSVYSbSSXO8RLw=";
     };
